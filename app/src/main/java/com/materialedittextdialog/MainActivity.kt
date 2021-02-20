be773ef7a5f3a97af44c84c69c2b20c2535a2bf3
VISHAL <Vishal@58039@gmail.com>
2021-02-20 12:35:15 +0530

    Initial Commit Fab 20

--- app/src/main/java/com/materialedittextdialog/MainActivity.kt
+++ app/src/main/java/com/materialedittextdialog/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 setNegativeButton("Cancel"){text,isValidated, dialog ->
-                    dialog.dismiss() // manually dismiss dialog is important
+                    dialog.dismiss() // manually dismiss dialog is important.
                 }
             }
             editTextDialog.show()
